b1b15776444d91a5fa9049cbbec7dd9a71e8e271
JakePowell7 <jakespowell7@gmail.com>
2017-04-28 12:48:26 +0100

    Added Mock

---
+++ MockAssessment/MockAssessment/Candidate/Holidays/Holidays/Holidays/Customers.Designer.vb
@@ -0,0 +1,208 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Customers
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblID = New System.Windows.Forms.Label()
+        Me.lblDOB = New System.Windows.Forms.Label()
+        Me.lblPhoneNumber = New System.Windows.Forms.Label()
+        Me.lblEmail = New System.Windows.Forms.Label()
+        Me.lblLastName = New System.Windows.Forms.Label()
+        Me.lblFirstName = New System.Windows.Forms.Label()
+        Me.cmdSave = New System.Windows.Forms.Button()
+        Me.cmdSearch = New System.Windows.Forms.Button()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.txtPhoneNumber = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.dtpDOB = New System.Windows.Forms.DateTimePicker()
+        Me.cmdNewSearch = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblID
+        '
+        Me.lblID.AutoSize = True
+        Me.lblID.Location = New System.Drawing.Point(93, 40)
+        Me.lblID.Name = "lblID"
+        Me.lblID.Size = New System.Drawing.Size(74, 20)
+        Me.lblID.TabIndex = 0
+        Me.lblID.Text = "Client ID:"
+        '
+        'lblDOB
+        '
+        Me.lblDOB.AutoSize = True
+        Me.lblDOB.Location = New System.Drawing.Point(64, 261)
+        Me.lblDOB.Name = "lblDOB"
+        Me.lblDOB.Size = New System.Drawing.Size(103, 20)
+        Me.lblDOB.TabIndex = 1
+        Me.lblDOB.Text = "Date of Birth:"
+        '
+        'lblPhoneNumber
+        '
+        Me.lblPhoneNumber.AutoSize = True
+        Me.lblPhoneNumber.Location = New System.Drawing.Point(48, 216)
+        Me.lblPhoneNumber.Name = "lblPhoneNumber"
+        Me.lblPhoneNumber.Size = New System.Drawing.Size(119, 20)
+        Me.lblPhoneNumber.TabIndex = 2
+        Me.lblPhoneNumber.Text = "Phone Number:"
+        '
+        'lblEmail
+        '
+        Me.lblEmail.AutoSize = True
+        Me.lblEmail.Location = New System.Drawing.Point(52, 169)
+        Me.lblEmail.Name = "lblEmail"
+        Me.lblEmail.Size = New System.Drawing.Size(115, 20)
+        Me.lblEmail.TabIndex = 3
+        Me.lblEmail.Text = "Email Address:"
+        '
+        'lblLastName
+        '
+        Me.lblLastName.AutoSize = True
+        Me.lblLastName.Location = New System.Drawing.Point(77, 126)
+        Me.lblLastName.Name = "lblLastName"
+        Me.lblLastName.Size = New System.Drawing.Size(90, 20)
+        Me.lblLastName.TabIndex = 4
+        Me.lblLastName.Text = "Last Name:"
+        '
+        'lblFirstName
+        '
+        Me.lblFirstName.AutoSize = True
+        Me.lblFirstName.Location = New System.Drawing.Point(77, 81)
+        Me.lblFirstName.Name = "lblFirstName"
+        Me.lblFirstName.Size = New System.Drawing.Size(90, 20)
+        Me.lblFirstName.TabIndex = 5
+        Me.lblFirstName.Text = "First Name:"
+        '
+        'cmdSave
+        '
+        Me.cmdSave.Location = New System.Drawing.Point(97, 316)
+        Me.cmdSave.Name = "cmdSave"
+        Me.cmdSave.Size = New System.Drawing.Size(109, 43)
+        Me.cmdSave.TabIndex = 6
+        Me.cmdSave.Text = "Save details"
+        Me.cmdSave.UseVisualStyleBackColor = True
+        '
+        'cmdSearch
+        '
+        Me.cmdSearch.Location = New System.Drawing.Point(238, 316)
+        Me.cmdSearch.Name = "cmdSearch"
+        Me.cmdSearch.Size = New System.Drawing.Size(109, 42)
+        Me.cmdSearch.TabIndex = 7
+        Me.cmdSearch.Text = "Search"
+        Me.cmdSearch.UseVisualStyleBackColor = True
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(173, 37)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(200, 26)
+        Me.txtID.TabIndex = 8
+        '
+        'txtPhoneNumber
+        '
+        Me.txtPhoneNumber.Location = New System.Drawing.Point(173, 210)
+        Me.txtPhoneNumber.Name = "txtPhoneNumber"
+        Me.txtPhoneNumber.Size = New System.Drawing.Size(200, 26)
+        Me.txtPhoneNumber.TabIndex = 9
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(173, 169)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(200, 26)
+        Me.txtEmail.TabIndex = 10
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(173, 123)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(200, 26)
+        Me.txtLastName.TabIndex = 11
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(173, 78)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(200, 26)
+        Me.txtFirstName.TabIndex = 12
+        '
+        'dtpDOB
+        '
+        Me.dtpDOB.Location = New System.Drawing.Point(173, 255)
+        Me.dtpDOB.Name = "dtpDOB"
+        Me.dtpDOB.Size = New System.Drawing.Size(200, 26)
+        Me.dtpDOB.TabIndex = 13
+        '
+        'cmdNewSearch
+        '
+        Me.cmdNewSearch.Location = New System.Drawing.Point(238, 317)
+        Me.cmdNewSearch.Name = "cmdNewSearch"
+        Me.cmdNewSearch.Size = New System.Drawing.Size(109, 42)
+        Me.cmdNewSearch.TabIndex = 14
+        Me.cmdNewSearch.Text = "Search"
+        Me.cmdNewSearch.UseVisualStyleBackColor = True
+        '
+        'Customers
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(426, 402)
+        Me.Controls.Add(Me.cmdNewSearch)
+        Me.Controls.Add(Me.dtpDOB)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.txtPhoneNumber)
+        Me.Controls.Add(Me.txtID)
+        Me.Controls.Add(Me.cmdSearch)
+        Me.Controls.Add(Me.cmdSave)
+        Me.Controls.Add(Me.lblFirstName)
+        Me.Controls.Add(Me.lblLastName)
+        Me.Controls.Add(Me.lblEmail)
+        Me.Controls.Add(Me.lblPhoneNumber)
+        Me.Controls.Add(Me.lblDOB)
+        Me.Controls.Add(Me.lblID)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Customers"
+        Me.Text = "Customers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblID As System.Windows.Forms.Label
+    Friend WithEvents lblDOB As System.Windows.Forms.Label
+    Friend WithEvents lblPhoneNumber As System.Windows.Forms.Label
+    Friend WithEvents lblEmail As System.Windows.Forms.Label
+    Friend WithEvents lblLastName As System.Windows.Forms.Label
+    Friend WithEvents lblFirstName As System.Windows.Forms.Label
+    Friend WithEvents cmdSave As System.Windows.Forms.Button
+    Friend WithEvents cmdSearch As System.Windows.Forms.Button
+    Friend WithEvents txtID As System.Windows.Forms.TextBox
+    Friend WithEvents txtPhoneNumber As System.Windows.Forms.TextBox
+    Friend WithEvents txtEmail As System.Windows.Forms.TextBox
+    Friend WithEvents txtLastName As System.Windows.Forms.TextBox
+    Friend WithEvents txtFirstName As System.Windows.Forms.TextBox
+    Friend WithEvents dtpDOB As System.Windows.Forms.DateTimePicker
+    Friend WithEvents cmdNewSearch As System.Windows.Forms.Button
+End Class
---
+++ MockAssessment/MockAssessment/Candidate/Holidays/Holidays/Holidays/Customers.vb
@@ -0,0 +1,158 @@
+﻿Imports System.IO
+
+Public Class Customers
+    Private Structure Customer
+        Public ID As String
+        Public FirstName As String
+        Public LastName As String
+        Public Email As String                  'Creating the structure that will hold the  data.
+        Public PhoneNumber As String
+        Public DOB As String
+    End Structure
+
+    Public Function Wipe() 'this will clear any text from within the textboxes
+        txtFirstName.Text = ""
+        txtLastName.Text = ""
+        txtEmail.Text = ""
+        txtPhoneNumber.Text = ""
+    End Function
+
+    Public Function ShowControls() 'this will show the controls to the user when they need to enter data
+        lblFirstName.Visible = True
+        lblLastName.Visible = True
+        lblEmail.Visible = True
+        lblPhoneNumber.Visible = True
+        lblDOB.Visible = True
+        txtFirstName.Visible = True
+        txtLastName.Visible = True
+        txtEmail.Visible = True
+        txtPhoneNumber.Visible = True
+        dtpDOB.Visible = True
+    End Function
+
+    Public Function hideControls() 'Likewise, this will hide the controls from the user
+        lblFirstName.Visible = False
+        lblLastName.Visible = False
+        lblEmail.Visible = False
+        lblPhoneNumber.Visible = False
+        lblDOB.Visible = False
+        txtFirstName.Visible = False
+        txtLastName.Visible = False
+        txtEmail.Visible = False
+        txtPhoneNumber.Visible = False
+        dtpDOB.Visible = False
+    End Function
+
+    Private Sub Holidays_Load() Handles MyBase.Load
+        If Dir$("customerdetails.txt") = "" Then
+            Dim sw As New StreamWriter("customerdetails.txt", True)    'This makes sure there is actually a database to enter/read data. If not, it creates a new blank one.
+            sw.WriteLine("")
+            sw.Close()
+            MsgBox("A new file has been created", vbExclamation, "Warning!")
+        End If
+
+        Dim ReadLines As String() = File.ReadAllLines(Dir$("customerdetails.txt")) 'This will make sure that the Client ID is correct by adding one to what it last finds in the text file
+        txtID.Text = Val(Trim(Mid(ReadLines.Last, 1, 20))) + 1
+
+        txtID.Enabled = False
+        cmdNewSearch.Visible = False
+    End Sub
+
+    Private Sub cmdSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdSave.Click
+        If txtFirstName.Text = "" Then 'This will be where I do my validation
+            MessageBox.Show("Please enter your first name.") 'this will test if there is any text entered into a textbox
+            Exit Sub
+        ElseIf txtLastName.Text = "" Then
+            MessageBox.Show("Please enter your last name")
+            Exit Sub
+        ElseIf txtEmail.Text = "" Then
+            MessageBox.Show("Please enter your email.")
+            Exit Sub
+        ElseIf txtPhoneNumber.TextLength = 0 Then
+            MessageBox.Show("Please enter your phone number.")
+            Exit Sub
+        ElseIf txtPhoneNumber.TextLength <> 11 Then 'this will test the length of text within the textbox to see if it meets the requirement, if not, it gives an error
+            MessageBox.Show("Invalid phone number")
+            Exit Sub
+        ElseIf dtpDOB.Value.ToString("yyyy") = 2015 Then
+            MessageBox.Show("Year date is higher than 2015")
+            Exit Sub
+        ElseIf txtEmail.Text.Contains("@") = False Then 'this will test to see if the user's email address contains an @ symbol.
+            MessageBox.Show("Please enter a vaild email address")
+            Exit Sub
+        End If
+
+        If txtLastName.TextLength > 15 Then 'If the text is longer than the length allowed, it will shorten it to its specified amount
+            MessageBox.Show("Your last name is too long and has been shorten to 15 characters")
+        ElseIf txtFirstName.TextLength > 12 Then
+            MessageBox.Show("Your first name is too long and has been shorten to 12 characters")
+        End If
+
+        Dim CustomerData As New Customer
+        Dim DOB As String
+        Dim sw As New System.IO.StreamWriter("customerdetails.txt", True)
+        CustomerData.ID = LSet(txtID.Text, 20)
+        CustomerData.FirstName = LSet(txtFirstName.Text, 12)
+        CustomerData.LastName = LSet(txtLastName.Text, 15)
+        CustomerData.Email = LSet(txtEmail.Text, 35)                      'Filling the structure with data.
+        CustomerData.PhoneNumber = LSet(txtPhoneNumber.Text, 15)
+        DOB = dtpDOB.Value.ToString("dd/MM/yyyy")
+        CustomerData.DOB = LSet(DOB, 10)
+        sw.WriteLine(CustomerData.ID & CustomerData.FirstName & CustomerData.LastName & CustomerData.Email & CustomerData.PhoneNumber & CustomerData.DOB)
+        sw.Close()                                                                  'Always need to close afterwards
+        MsgBox("File Saved!")
+        Wipe()
+
+        Dim NewID As Integer 'This will chance the Client ID everytime the save button is pressed.
+        NewID = Val(txtID.Text) + 1
+        txtID.Text = NewID
+    End Sub
+
+    Private Sub cmdSearch_Click(sender As System.Object, e As System.EventArgs) Handles cmdSearch.Click
+        cmdNewSearch.Visible = True 'This will show another button which will look like the button has changed code
+        hideControls()
+        Wipe()
+        txtID.Enabled = True
+        txtID.Text = ""
+    End Sub
+
+    Private Sub cmdNewSearch_Click(sender As System.Object, e As System.EventArgs) Handles cmdNewSearch.Click
+        If txtID.Text = "" Then
+            MessageBox.Show("Please Enter the Client ID you are searching for")
+        Else
+            Dim ClientID As Integer
+            ClientID = Val(txtID.Text) 'This will be what is used to search through the text document
+            Dim CustomerData() As String = File.ReadAllLines(Dir$("customerdetails.txt"))
+            Try
+                For I = 0 To UBound(CustomerData)
+                    If ClientID = Val(Trim(Mid(CustomerData(I), 1, 20))) Then 'if a integer with the same value of ClientID is found, then it will load the data from the text document
+                        txtFirstName.Text = Trim(Mid(CustomerData(I), 21, 12))
+                        txtLastName.Text = Trim(Mid(CustomerData(I), 33, 15))
+                        txtEmail.Text = Trim(Mid(CustomerData(I), 48, 35))
+                        txtPhoneNumber.Text = Trim(Mid(CustomerData(I), 83, 11))
+                        dtpDOB.Value = CDate(Trim(Mid(CustomerData(I), 99, 10)))
+                        ShowControls()
+                        MessageBox.Show("Customer details have been found")
+                        Exit Sub
+                    End If
+                Next
+                MessageBox.Show("Failed to find the Client ID")
+            Catch
+                MessageBox.Show("Unable to load the Customer Details")
+            End Try
+        End If
+    End Sub
+
+    Private Sub txtFirstName_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles txtFirstName.KeyDown
+        Select Case e.KeyCode 'By doing this, i am able to type check to make sure that no integers are entered into the first and last name text box
+            Case Keys.D1, Keys.D2, Keys.D3, Keys.D4, Keys.D5, Keys.D6, Keys.D7, Keys.D8, Keys.D9, Keys.D0, Keys.LControlKey
+                SendKeys.Send("{Backspace}")
+        End Select
+    End Sub
+    Private Sub txtLastName_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles txtLastName.KeyDown
+        Select Case e.KeyCode
+            Case Keys.D1, Keys.D2, Keys.D3, Keys.D4, Keys.D5, Keys.D6, Keys.D7, Keys.D8, Keys.D9, Keys.D0, Keys.LControlKey
+                SendKeys.Send("{Backspace}")
+        End Select
+    End Sub
+End Class
